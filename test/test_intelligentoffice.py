bc5676dc6299342a5702fb163c8f41dd0eb7fd8a
VanniMaceria <andreabucci123@gmail.com>
2025-12-18 14:30:36 +0100

    Green phase user story #1 - third quadrant

--- test/test_intelligentoffice.py
+++ test/test_intelligentoffice.py
@@ -44,6 +44,13 @@ class TestIntelligentOffice(unittest.TestCase):
         outcome = office.check_quadrant_occupancy(office.INFRARED_PIN3)
         self.assertTrue(outcome)
 
+    @patch.object(GPIO, "input")
+    def test_check_third_quadrant_is_not_occupied(self, infrared: Mock):
+        infrared.return_value = False
+        office = IntelligentOffice()
+        outcome = office.check_quadrant_occupancy(office.INFRARED_PIN3)
+        self.assertFalse(outcome)
+
 
 
 
